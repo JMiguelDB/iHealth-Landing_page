aa8d6db2acd5245d6f1690378a96f79ead426c69
José Miguel <jose.miguel.db.17894@gmail.com>
2017-10-26 14:53:08 +0200

    Landing page funcionando

--- public/index-ctrl.js
+++ public/index-ctrl.js
@@ -1,9 +1,9 @@
-angular.module("iHealthApp", ["ngRoute"])
-    .config(function($routeProvider){
-        $routeProvider
-            .when("/",{
-                //controller: "home-ctrl",
-                templateUrl: "views/home.html",
-                access: {restricted:false}
-            })
+var app = angular.module('iHealthApp', ["ngRoute"]);
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+    .when("/", {
+        //controller: "Auth0-ctrl",
+        templateUrl: "views/home.html"
     });
+    $locationProvider.html5Mode(true);
+});
